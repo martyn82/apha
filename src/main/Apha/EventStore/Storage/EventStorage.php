af57ff41a97431fd538a9170855705b616a85c7a
Martijn Endenburg <martijn.endenburg@gmail.com>
2016-02-13 00:26:44 +0100

    Create EventStorage interface.

---
+++ src/main/Apha/EventStore/Storage/EventStorage.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace Apha\EventStore\Storage;
+
+use Apha\EventStore\EventDescriptor;
+
+interface EventStorage
+{
+    /**
+     * @param string $identity
+     * @return bool
+     */
+    public function contains(string $identity) : bool;
+
+    /**
+     * @param EventDescriptor $event
+     * @return bool
+     */
+    public function append(EventDescriptor $event) : bool;
+
+    /**
+     * @param string $identity
+     * @return EventDescriptor[]
+     */
+    public function find(string $identity) : array;
+
+    /**
+     * @return string[]
+     */
+    public function findIdentities() : array;
+}
