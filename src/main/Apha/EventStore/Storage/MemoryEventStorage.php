09e546cb0f2470013bc2d264c603626e64c30a80
Martijn Endenburg <martijn.endenburg@gmail.com>
2016-02-13 15:22:59 +0100

    Implement memory event storage.

---
+++ src/main/Apha/EventStore/Storage/MemoryEventStorage.php
@@ -0,0 +1,63 @@
+<?php
+declare(strict_types=1);
+
+namespace Apha\EventStore\Storage;
+
+use Apha\EventStore\EventDescriptor;
+
+class MemoryEventStorage implements EventStorage
+{
+    /**
+     * @var array
+     */
+    private $data = [];
+
+    /**
+     * @param string $identity
+     * @return bool
+     */
+    public function contains(string $identity) : bool
+    {
+        return array_key_exists($identity, $this->data);
+    }
+
+    /**
+     * @param EventDescriptor $event
+     * @return bool
+     */
+    public function append(EventDescriptor $event) : bool
+    {
+        if (!$this->contains($event->getIdentity())) {
+            $this->data[$event->getIdentity()] = [];
+        }
+
+        $this->data[$event->getIdentity()][] = $event->toArray();
+        return true;
+    }
+
+    /**
+     * @param string $identity
+     * @return EventDescriptor[]
+     */
+    public function find(string $identity) : array
+    {
+        if (!$this->contains($identity)) {
+            return [];
+        }
+
+        return array_map(
+            function (array $event) {
+                return EventDescriptor::reconstructFromArray($event);
+            },
+            $this->data[$identity]
+        );
+    }
+
+    /**
+     * @return string[]
+     */
+    public function findIdentities() : array
+    {
+        return array_keys($this->data);
+    }
+}
---
+++ src/test/Apha/EventStore/Storage/MemoryEventStorageTest.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace Apha\EventStore\Storage;
+
+use Apha\EventStore\EventDescriptor;
+
+class MemoryEventStorageTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function containsReturnsFalseIfIdentityDoesNotExistInStorage()
+    {
+        $identity = 'nonexistent';
+        $storage = new MemoryEventStorage();
+        self::assertFalse($storage->contains($identity));
+    }
+
+    /**
+     * @test
+     */
+    public function containsReturnsTrueIfIdentityExistsInStorage()
+    {
+        $identity = 'existing';
+        $event = EventDescriptor::record(
+            $identity,
+            'someevent',
+            '{}',
+            1
+        );
+
+        $storage = new MemoryEventStorage();
+        $storage->append($event);
+
+        self::assertTrue($storage->contains($identity));
+    }
+
+    /**
+     * @test
+     */
+    public function appendAcceptsTheFirstEventForAnIdentity()
+    {
+        $identity = 'someidentity';
+        $event = EventDescriptor::record(
+            $identity,
+            'someevent',
+            '{}',
+            1
+        );
+
+        $storage = new MemoryEventStorage();
+        self::assertTrue($storage->append($event));
+    }
+
+    /**
+     * @test
+     */
+    public function appendAcceptsTheNextEventForAnIdentity()
+    {
+        $identity = 'someidentity';
+
+        $event1 = EventDescriptor::record(
+            $identity,
+            'someevent',
+            '{}',
+            1
+        );
+
+        $event2 = EventDescriptor::record(
+            $identity,
+            'someevent',
+            '{}',
+            2
+        );
+
+        $storage = new MemoryEventStorage();
+
+        self::assertTrue($storage->append($event1));
+        self::assertTrue($storage->append($event2));
+    }
+
+    /**
+     * @test
+     */
+    public function findWillReturnEmptyResultIfIdentityDoesNotExist()
+    {
+        $storage = new MemoryEventStorage();
+        self::assertEmpty($storage->find('nonexistent'));
+    }
+
+    /**
+     * @test
+     */
+    public function findWillReturnAllEventsInOrderForIdentity()
+    {
+        $identity = 'someidentity';
+        $event1 = EventDescriptor::record(
+            $identity,
+            'someevent1',
+            '{}',
+            1
+        );
+        $event2 = EventDescriptor::record(
+            $identity,
+            'someevent2',
+            '{}',
+            2
+        );
+
+        $storage = new MemoryEventStorage();
+        $storage->append($event1);
+        $storage->append($event2);
+
+        $events = $storage->find($identity);
+
+        self::assertCount(2, $events);
+        self::assertEquals($event1, $events[0]);
+        self::assertEquals($event2, $events[1]);
+    }
+
+    /**
+     * @test
+     */
+    public function findIdentitiesWillRetrieveAllKnownIdentities()
+    {
+        $identity1 = 'someidentity';
+        $identity2 = 'someotheridentity';
+
+        $event1 = EventDescriptor::record(
+            $identity1,
+            'someevent',
+            '{}',
+            1
+        );
+        $event2 = EventDescriptor::record(
+            $identity2,
+            'someevent',
+            '{}',
+            1
+        );
+
+        $storage = new MemoryEventStorage();
+        $storage->append($event1);
+        $storage->append($event2);
+
+        $identities = $storage->findIdentities();
+
+        self::assertCount(2, $identities);
+        self::assertContains($identity1, $identities);
+        self::assertContains($identity2, $identities);
+    }
+}
