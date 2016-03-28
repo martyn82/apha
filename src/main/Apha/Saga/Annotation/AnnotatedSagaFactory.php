2b183607412828629b156ac865fc4e879bf388f1
Martijn Endenburg <martijn.endenburg@gmail.com>
2016-03-28 15:55:26 +0200

    Add annotated saga factory.

---
+++ src/main/Apha/Saga/Annotation/AnnotatedSagaFactory.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types = 1);
+
+namespace Apha\Saga\Annotation;
+
+use Apha\Annotations\ParameterResolver;
+use Apha\Domain\Identity;
+use Apha\Saga\AssociationValue;
+use Apha\Saga\AssociationValues;
+use Apha\Saga\Saga;
+use Apha\Saga\SagaFactory;
+
+class AnnotatedSagaFactory implements SagaFactory
+{
+    /**
+     * @var ParameterResolver
+     */
+    private $parameterResolver;
+
+    /**
+     * @param ParameterResolver $parameterResolver
+     */
+    public function __construct(ParameterResolver $parameterResolver)
+    {
+        $this->parameterResolver = $parameterResolver;
+    }
+
+    /**
+     * @param string $sagaType
+     * @param Identity $identity
+     * @param AssociationValues $associationValues
+     * @return Saga
+     * @throws \InvalidArgumentException
+     */
+    public function createSaga(string $sagaType, Identity $identity, AssociationValues $associationValues): Saga
+    {
+        if (!$this->supports($sagaType)) {
+            throw new \InvalidArgumentException("This factory does not support Sagas of type '{$sagaType}'.");
+        }
+
+        /* @var $saga AnnotatedSaga */
+        $saga = new $sagaType($identity);
+        $saga->setParameterResolver($this->parameterResolver);
+
+        /* @var $associationValue AssociationValue */
+        foreach ($associationValues as $associationValue) {
+            $saga->associateWith($associationValue);
+        }
+
+        return $saga;
+    }
+
+    /**
+     * @param string $sagaType
+     * @return bool
+     */
+    public function supports(string $sagaType): bool
+    {
+        return is_subclass_of($sagaType, AnnotatedSaga::class, true);
+    }
+}
