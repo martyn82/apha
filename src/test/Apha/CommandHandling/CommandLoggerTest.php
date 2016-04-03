72e204fa708fd1430f7ebd05c03e64da9c97e86b
Martijn Endenburg <martijn.endenburg@gmail.com>
2016-04-03 13:38:35 +0200

    Add tests.

---
+++ src/test/Apha/CommandHandling/CommandLoggerTest.php
@@ -0,0 +1,64 @@
+<?php
+declare(strict_types = 1);
+
+namespace Apha\CommandHandling;
+
+use Apha\Message\Command;
+use Psr\Log\LoggerInterface;
+
+class CommandLoggerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function beforeDispatchIsLogged()
+    {
+        $command = $this->getMockBuilder(Command::class)
+            ->getMock();
+
+        $logger = $this->getMockBuilder(LoggerInterface::class)
+            ->getMock();
+
+        $logger->expects(self::once())
+            ->method('info');
+
+        $commandLogger = new CommandLogger($logger);
+        $commandLogger->onBeforeDispatch($command);
+    }
+
+    /**
+     * @test
+     */
+    public function dispatchSuccessfulIsLogged()
+    {
+        $command = $this->getMockBuilder(Command::class)
+            ->getMock();
+
+        $logger = $this->getMockBuilder(LoggerInterface::class)
+            ->getMock();
+
+        $logger->expects(self::once())
+            ->method('info');
+
+        $commandLogger = new CommandLogger($logger);
+        $commandLogger->onDispatchSuccessful($command);
+    }
+
+    /**
+     * @test
+     */
+    public function dispatchFailedIsLogged()
+    {
+        $command = $this->getMockBuilder(Command::class)
+            ->getMock();
+
+        $logger = $this->getMockBuilder(LoggerInterface::class)
+            ->getMock();
+
+        $logger->expects(self::once())
+            ->method('error');
+
+        $commandLogger = new CommandLogger($logger);
+        $commandLogger->onDispatchFailed($command, new \Exception('Something is wrong.'));
+    }
+}
---
+++ src/test/Apha/EventHandling/EventLoggerTest.php
@@ -0,0 +1,64 @@
+<?php
+declare(strict_types = 1);
+
+namespace Apha\EventHandling;
+
+use Apha\Message\Event;
+use Psr\Log\LoggerInterface;
+
+class EventLoggerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function beforeDispatchIsLogged()
+    {
+        $event = $this->getMockBuilder(Event::class)
+            ->getMock();
+
+        $logger = $this->getMockBuilder(LoggerInterface::class)
+            ->getMock();
+
+        $logger->expects(self::once())
+            ->method('info');
+
+        $eventLogger = new EventLogger($logger);
+        $eventLogger->onBeforeDispatch($event);
+    }
+
+    /**
+     * @test
+     */
+    public function dispatchSuccessfulIsLogged()
+    {
+        $event = $this->getMockBuilder(Event::class)
+            ->getMock();
+
+        $logger = $this->getMockBuilder(LoggerInterface::class)
+            ->getMock();
+
+        $logger->expects(self::once())
+            ->method('info');
+
+        $eventLogger = new EventLogger($logger);
+        $eventLogger->onDispatchSuccessful($event);
+    }
+
+    /**
+     * @test
+     */
+    public function dispatchFailedIsLogged()
+    {
+        $event = $this->getMockBuilder(Event::class)
+            ->getMock();
+
+        $logger = $this->getMockBuilder(LoggerInterface::class)
+            ->getMock();
+
+        $logger->expects(self::once())
+            ->method('warning');
+
+        $eventLogger = new EventLogger($logger);
+        $eventLogger->onDispatchFailed($event);
+    }
+}
